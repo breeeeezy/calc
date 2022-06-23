c96eaec72e215b99b74837fb20786d66118ba902
breeeeezy <shreyasmamdyal@gmail.com>
2022-06-23 12:35:59 +1000

    compacted calc

--- week7.py
+++ week7.py
@@ -17,33 +17,19 @@ def _get_number(i):
 def _print_result(first_number, second_number, operation, result):
     print(f"{first_number} {operation} {second_number} = {result}")
 
+def calculate(operation):
+    _introduction(operation)
+    num1 = _get_number(1)
+    num2 = _get_number(2)
+
+    if operation == "add":
+        result = num1+num2
+    if operation == "subtract":
+        result = num1-num2
+    if operation == "multiply":
+        result = num1*num2
+    if operation == "divide":
+        result = num1/num2
+
+    _print_result(num1, num2, operation, result)
 #---------------#
-
-def add_numbers():
-    _introduction("add")
-    num1 = _get_number(1)
-    num2 = _get_number(2)
-    result = num1+num2
-    _print_result(num1, num2, "+", result)
-
-def subtract_numbers():
-    _introduction("subtract")
-    num1 = _get_number(1)
-    num2 = _get_number(2)
-    result = num1-num2
-    _print_result(num1, num2, "-", result)
-
-def multiply_numbers():
-    _introduction("multiply")
-    num1 = _get_number(1)
-    num2 = _get_number(2)
-    result = num1*num2
-    _print_result(num1, num2, "*", result)
-
-def divide_numbers():
-    _introduction("divide")
-    num1 = _get_number(1)
-    num2 = _get_number(2)
-    result = num1/num2
-    _print_result(num1, num2, "/", result)
-
